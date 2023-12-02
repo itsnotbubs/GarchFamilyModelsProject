792d14f49618b1713c64c366e33e1f70ffe1640c
itsnotbubs <100372323+itsnotbubs@users.noreply.github.com>
2023-12-01 17:49:58 -1000

    Updated R file
    corrected former commit

--- FIN4011Project.R
+++ FIN4011Project.R
@@ -1,78 +1,87 @@
 # Install and Load Necessary Libraries
-install.packages(c("quantmod", "rugarch", "forecast", "tseries", "urca"))
+install.packages(c("quantmod", "rugarch", "forecast", "tseries", "urca", "FinTS", "ggplot2"))
 library(quantmod)
 library(rugarch)
 library(forecast)
 library(tseries)
 library(urca)
+library(FinTS)
+library(ggplot2)
 
 # Download and Prepare Data from Yahoo Finance
 getSymbols("SPY", src = "yahoo", from = "2010-01-01", to = "2023-01-01")
 prices <- Cl(SPY) # Closing prices of SPY
-returns <- dailyReturn(prices) # Daily returns
+returns <- dailyReturn(prices) 
+mean_volatility <- sd(returns)
+print(mean_volatility)*100
+
+# Histogram of Daily Returns
+ggplot(data = data.frame(returns = coredata(returns)), aes(x = returns)) +
+  geom_histogram(bins = 50, fill = "blue", color = "black", alpha = 0.7) +
+  labs(title = "Histogram of Daily Returns for SPY", x = "Daily Returns", y = "Frequency")
 
 # Check for Stationarity and Unit Roots
-adf.test(returns, alternative = "stationary") # Augmented Dickey-Fuller Test
-
-# Examine ACF and PACF of Returns
-acf(coredata(returns), main="ACF of Returns") 
-pacf(coredata(returns), main="PACF of Returns")
+adf_test_results <- adf.test(returns, alternative = "stationary") # Augmented Dickey-Fuller Test
 
 # Fit a Mean Model  (ARIMA)
 fit_arima <- auto.arima(returns) #auto.arima iterates through different models to achieve the "best fit" based on AIC/BIC
 
-summary(fit_arima)
+# Ljung-Box Test for serial correlation
+box_test_results <- Box.test(residuals(fit_arima), type = "Ljung-Box")
 
-Box.test(residuals(fit_arima), type = "Ljung-Box") # Ljung-Box Test for serial correlation
+# Test to see if ARCH effects are present
+arch_test_results <- ArchTest(returns)
+print(arch_test_results)
 
-#PACF for order determination of ARCH effects model
-residuals_arima <- residuals(fit_arima)
+garch_mod <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
+                           mean.model = list(armaOrder = auto.arima(returns)$arma, include.mean = TRUE))
 
-pacf(residuals_arima, main="PACF of ARIMA Model Residuals")
-# Check for ARCH Effects using squared residuals
-residuals_squared <- residuals_arima^2
+garch_fit <- ugarchfit(garch_mod, data = returns)
 
-lagged_resid_squared <- lag(residuals_squared, k=1)
+garch_resid <- residuals(garch_fit, standardize=TRUE)
 
-# Align the lengths by removing NA values
-residuals_squared <- residuals_squared[-1]  # Remove the first value which is NA in lagged series
-lagged_resid_squared <- lagged_resid_squared[-length(lagged_resid_squared)]  # Remove the last value
+print(garch_fit)
 
-lm_model <- lm(residuals_squared ~ lagged_resid_squared)
-summary(lm_model)
+# Extract conditional volatility
+garch_volatility <- sigma(garch_fit)
 
-# Split the Data into Training and Test Sets
-split_index <- round(length(returns) * 0.8)
-train_returns <- returns[1:split_index]
-test_returns <- returns[(split_index+1):length(returns)]
+# Create a time series object
+garch_vol_ts <- ts(garch_volatility, start = c(2010), frequency = 365)
 
-# Fit a Basic GARCH Model
-spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-                   mean.model = list(armaOrder = c(0, 0), include.mean = TRUE))
-garch_fit <- ugarchfit(spec = spec, data = train_returns)
-
-# Forecasting and Evaluation
-garch_forecast <- ugarchforecast(garch_fit, n.ahead = 10)
-forecast_accuracy <- accuracy(garch_forecast, test_returns) # Evaluate forecast accuracy
-
-# Naive Forecast for Comparison
-naive_forecast <- mean(train_returns^2)
-
-# Model Diagnostics and Further Analysis
-summary(garch_fit) # Model summary
-acf(residuals(garch_fit)^2, main="ACF of Squared Residuals") # Check for autocorrelation in residuals
-
-# Experiment with Other Models like EGARCH
-egarch_spec <- ugarchspec(variance.model = list(model = "eGARCH", garchOrder = c(1, 1)),
-                          mean.model = list(armaOrder = c(0, 0), include.mean = TRUE))
-egarch_fit <- ugarchfit(spec = egarch_spec, data = train_returns)
-
-# Conclude with Analysis
-# Compare the models, discuss the findings, and draw conclusions about their effectiveness
+# Plot the GARCH volatility
+plot(garch_vol_ts, xlab = "Year", ylab = "Volatility", main = "GARCH Volatility [1,1]")
 
 
+#egarch portion
+egarch_mod <- ugarchspec(variance.model = list(model = "eGARCH", garchOrder = c(1, 1)),
+                          mean.model = list(armaOrder = auto.arima(returns)$arma, include.mean = TRUE))
+
+egarch_fit <- ugarchfit(egarch_mod, data =returns)
+
+print(egarch_fit)
+
+# Extract conditional volatility
+egarch_volatility <- sigma(egarch_fit)
+
+# Create a time series object
+egarch_vol_ts <- ts(egarch_volatility, start = c(2010), frequency = 365)
+
+# Plot the EGARCH volatility
+plot(egarch_vol_ts, xlab = "Year", ylab = "Volatility", main = "EGARCH Volatility [1,1]")
 
 
+tgarch_mod <- ugarchspec(variance.model = list(model = "fGARCH",
+                          submodel = "TGARCH", garchOrder = c(1, 1)), mean.model = list(armaOrder = auto.arima(returns)$arma, 
+                                            include.mean = TRUE))
+tgarch_fit <- ugarchfit(tgarch_mod, data =returns)
 
+print(tgarch_fit)
 
+# Extract conditional volatility
+tgarch_volatility <- sigma(tgarch_fit)
 
+# Create a time series object
+tgarch_vol_ts <- ts(tgarch_volatility, start = c(2010), frequency = 365)
+
+# Plot the EGARCH volatility
+plot(tgarch_vol_ts, xlab = "Year", ylab = "Volatility", main = "tGARCH Volatility [1,1]")
